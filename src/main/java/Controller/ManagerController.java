718f351802f3fb15260c6c062e4ba859698ddb90
leejungdo <ljd100000@gmail.com>
2019-08-19 17:38:35 +0900

    이거 안되면 자살함

---
+++ src/main/java/Controller/ManagerController.java
@@ -0,0 +1,10 @@
+package Controller;
+
+@Controller
+public class ManagerController {
+	@Autowired
+	private CompRegService compRegService;
+	
+	@RequestMapping("/compReg")
+	public String 
+}
--- src/main/java/Model/DTO/item.java
+++ src/main/java/Model/DTO/item.java
@@ -1,5 +1,39 @@
 package Model.DTO;
-
+@SuppressWarnings("serial")
 public class item {
-
+	private int category;
+	private String trade;
+	private String pres;
+	private String manager;
+	private String ph;
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public String getTrade() {
+		return trade;
+	}
+	public void setTrade(String trade) {
+		this.trade = trade;
+	}
+	public String getPres() {
+		return pres;
+	}
+	public void setPres(String pres) {
+		this.pres = pres;
+	}
+	public String getManager() {
+		return manager;
+	}
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+	public String getPh() {
+		return ph;
+	}
+	public void setPh(String ph) {
+		this.ph = ph;
+	}
 }
