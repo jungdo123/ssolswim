4feab0de5578fd59ab35e993a3ab8291cb66442b
leejungdo <ljd100000@gmail.com>
2019-08-20 10:38:53 +0900

    몬가....몬가...바꿈...

--- src/main/java/Controller/ManagerController.java
+++ src/main/java/Controller/ManagerController.java
@@ -1,10 +1,23 @@
 package Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import Service.MemberListLookup;
+
 @Controller
 public class ManagerController {
-	@Autowired
-	private CompRegService compRegService;
+	//@Autowired
+	//private CompRegService compRegService;
+	//@Autowired
+	//private MemberListLookup memberListLookup;
+	
 	
 	@RequestMapping("/compReg")
-	public String 
+	public String terms() {
+		return "MemberView/terms";
+	}
+	
+	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -11,7 +11,7 @@
 		<title>Editorial by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="./main.css" />
 		<style>
       .table tr th{ text-align:center }
       .table tr td{ text-align:center }
